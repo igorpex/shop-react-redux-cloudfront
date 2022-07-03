148c9161f68b4e8428a7476a4031b512fae224cc
igorpex <igor.bogdanov@gmail.com>
2022-07-03 05:28:32 +0300

    feat: update import path

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -4,7 +4,7 @@ const API_PATHS = {
   product: 'https://a3loi3zsk8.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   // import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://a3loi3zsk8.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://cmirxr39ld.execute-api.eu-west-1.amazonaws.com/dev',
   // bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://a3loi3zsk8.execute-api.eu-west-1.amazonaws.com/dev',
   ts: 'https://a3loi3zsk8.execute-api.eu-west-1.amazonaws.com',
